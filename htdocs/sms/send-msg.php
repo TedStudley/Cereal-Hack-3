a8b6cfb54df06a1114e5ebdd8cf5fb847b77886c
Joe Hudson <Joe from Lexis Laptop>
2013-07-28 12:13:37 -0700

    sms: try a voice call

--- htdocs/sms/send-msg.php
+++ htdocs/sms/send-msg.php
@@ -6,10 +6,11 @@ $session = new Session();
 $to = "+".$session->getParameters("numberToDial"); 
 $name = $session->getParameters("customerName"); 
 $msg = $session->getParameters("msg"); 
+$msg = $session->getParameters("msg"); 
     
 $tropo = new Tropo(); 
     
-$tropo->call($to, array('network'=>'SMS')); 
+$tropo->call($to, array('network'=>'PSTN')); 
 $tropo->say("Hi ".$name.", ". $msg ); 
 
 return $tropo->RenderJson(); 
