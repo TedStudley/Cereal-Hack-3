6b80e18eb945e6194748150d8f3078db393aac37
Joe Hudson <Joe from Lexis Laptop>
2013-07-27 20:22:12 -0700

    sms: endless typo fixes

--- htdocs/sms/start-tropo.php
+++ htdocs/sms/start-tropo.php
@@ -7,26 +7,28 @@
 // voice URL = http://www.quickjobbr.com/sms/send-msg.php
 // text URL = http://www.quickjobbr.com/sms/send-msg.php
 // 
-$tropo_TEXT_TOK = "238554fd162c9244bfdaaf1f8f3a5f307d3941d9523a27312576f87f88367d525314e15b667dd47145ce0f4a";
+var_dump($_REQUEST);
+//$tropo_TEXT_TOK = "238554fd162c9244bfdaaf1f8f3a5f307d3941d9523a27312576f87f88367d525314e15b667dd47145ce0f4a";
 
-$tropo_api_url = "http://api.tropo.com/1.0/sessions";
 
-$fields = array(
-    'numberToDial'=>urlencode("+" . $_POST["numberToDial"]),
-    'customerName'=>urlencode($_POST["customerName"])
-    );
+//$tropo_api_url = "http://api.tropo.com/1.0/sessions";
 
-//url-ify the data for the GET
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string,'&');
+//$fields = array(
+    //'numberToDial'=>urlencode("+" . $_POST["numberToDial"]),
+    //'customerName'=>urlencode($_POST["customerName"])
+    //);
 
-// put the action and create at the end just so easier 
-// for human to read the url
-$text_msg_url = $tropo_api_url . "?" . $fields_string;
-$text_msg_url .= "&action=create";
-$text_msg_url .= "&token=$tropo_TEXT_TOK";
+////url-ify the data for the GET
+//foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+//rtrim($fields_string,'&');
 
-$html = file_get_contents($text_msg_url);
+//// put the action and create at the end just so easier 
+//// for human to read the url
+//$text_msg_url = $tropo_api_url . "?" . $fields_string;
+//$text_msg_url .= "&action=create";
+//$text_msg_url .= "&token=$tropo_TEXT_TOK";
+
+//$html = file_get_contents($text_msg_url);
 
 ?>
 
